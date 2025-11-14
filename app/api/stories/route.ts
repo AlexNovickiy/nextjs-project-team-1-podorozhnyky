fb4e6dfdffb9f264874f014910c9ca3d64219ec2
Alena Havalehko <126282451+AlenaHavaleshko@users.noreply.github.com>
2025-11-14 17:47:31 +0200

    Merge branch 'main' into add-story-form

--- app/api/stories/route.ts
+++ app/api/stories/route.ts
@@ -4,6 +4,7 @@ import { isAxiosError } from 'axios';
 import { NextResponse, NextRequest } from 'next/server';
 import { logErrorResponse } from '../_utils/utils';
 import { api } from '../api';
+import { cookies } from 'next/headers';
 
 export async function GET(req: NextRequest) {
   try {
@@ -36,5 +37,47 @@ export async function GET(req: NextRequest) {
       { error: 'Internal Server Error' },
       { status: 500 }
     );
+  }
+}
+
+
+export async function POST(request: Request) {
+ try {
+  const cookieStore = await cookies();
+
+  const token = cookieStore.get('authToken');
+  if(!token) {
+   return NextResponse.json(
+    { message: 'Unauthorized' }, 
+    { status: 401 });
+  }
+
+  const { title, description, shortDescription, category, storyImage } = await request.json();
+
+  const res = await api.post('/stories', {
+   title,
+   description,
+   shortDescription,
+   category,
+   storyImage
+  }, {
+   headers: {
+    Cookie: cookieStore.toString(),
+   }
+  });
+
+  return NextResponse.json(res.data, { status: res.status });
+ } catch (error) {
+    if (isAxiosError(error)) {
+      logErrorResponse(error.response?.data);
+      return NextResponse.json(
+        { error: error.message, response: error.response?.data },
+        { status: error.status }
+      );
+    }
+    logErrorResponse({ message: (error as Error).message });
+    return NextResponse.json(
+     { error: 'Internal Server Error' }, 
+     { status: 500 })
   }
 }
