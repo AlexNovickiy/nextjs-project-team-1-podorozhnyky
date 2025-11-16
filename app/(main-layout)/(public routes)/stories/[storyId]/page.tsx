1403cf37bdb1424d4c1847799bc5cc2040739570
Alina Slobodeniuk <112395038+AlinaSlobodeniuk@users.noreply.github.com>
2025-11-17 01:27:43 +0200

    StoryPage + StoryDetails

--- app/(main-layout)/(public routes)/stories/[storyId]/page.tsx
+++ app/(main-layout)/(public routes)/stories/[storyId]/page.tsx
@@ -1,5 +1,41 @@
+// import Popular from '@/components/Popular/Popular';
+import StoryDetails from '@/components/StoryDetails/StoryDetails';
+import { fetchStoryById } from '@/lib/api/serverApi';
+import css from './StoryPage.module.css';
+
 export type StoryPageProps = { params: { storyId: string } };
 
-export default function StoryPage({ params }: StoryPageProps) {
-  return <div>StoryPage</div>;
+export default async function StoryPage({ params }: StoryPageProps) {
+  const { storyId } = params;
+  const story = await fetchStoryById(storyId);
+
+  
+
+  //  const story = {
+  //   _id: storyId,
+  //   title: 'Венеція без туристів: маршрути для справжніх мандрівників',
+  //   img: '/images/avatar.jpg',
+  //   article: 'Петра — одне з нових чудес світу, і вживу воно вражає ще більше. Ми пройшли Сік — вузький каньйон, який веде до Скарбниці, і вразилися масштабом стародавнього міста, висіченого у скелях. День минув у дослідженні храмів, амфітеатрів і гробниць. А ввечері ми залишилися на нічне шоу, коли Петра освітлюється сотнями свічок. Атмосфера була настільки чарівною, що здавалося, час повернувся на тисячі років назад.',
+  //   category: { _id: '68fb50c80ae91338641121f0', name: 'Європа' },
+  //   ownerId: { _id: '6881563901add19ee16fd017', name: 'Дмитро Романенко', avatarUrl: '' },
+  //   date: '2025-01-10',
+  //   favoriteCount: 4,
+  //   createdAt: '2025-01-01',
+  //   updatedAt: '2025-01-05'
+  // };
+
+
+
+  // const popularStoriesRes = await fetchServerStories(3, 1, null);
+  // const popularStories = popularStoriesRes.data.filter(
+  //   (st) => st._id !== storyId
+  // );
+  return (
+    <section className={css.page} aria-label='story page'>
+      <h1 className={css.title}>{story.title}</h1>
+      <StoryDetails storyId={storyId} />
+      {/* <StoryDetails story={story} /> */}
+      {/* <Popular /> */}
+    </section>
+  );
 }
--- components/StoryDetails/StoryDetails.tsx
+++ components/StoryDetails/StoryDetails.tsx
@@ -1,5 +1,120 @@
+'use client';
+
+import { IStory } from '@/types/story';
+import { useEffect, useState } from 'react';
+import Loader from '../Loader/Loader';
+import Image from 'next/image';
+import toast from 'react-hot-toast';
+import { addFavorite, fetchStoryById } from '@/lib/api/clientApi';
+import css from './StoryDetails.module.css';
+
 const StoryDetails = ({ storyId }: { storyId: string }) => {
-  return <div>Story Details for ID: {storyId}</div>;
-}
+  const [story, setStory] = useState<IStory | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [saving, setSaving] = useState(false);
+
+  useEffect(() => {
+    const loadStory = async () => {
+      try {
+        const data = await fetchStoryById(storyId);
+        setStory(data);
+
+      } catch {
+        toast.error('Помилка завантаження історії:');
+      } finally {
+        setLoading(false);
+      }
+    };
+    loadStory();
+  }, [storyId]);
+
+  const handleSave = async () => {
+    if (!story) return;
+    setSaving(true);
+
+    try {
+      await addFavorite(story._id);
+
+      const update = await fetchStoryById(storyId);
+      setStory(update);
+      toast.success('Історію збережено!')
+    } catch {
+      toast.error('Помилка збереження історії:');
+    } finally {
+      setSaving(false);
+    }
+  };
+  if (loading) {
+    return <Loader />;
+  }
+
+  if (!story) {
+    return <p>Історію не знайдено</p>;
+  }
+
+// const StoryDetails = ({ story }: { story: IStory }) => {
+//   const [saving, setSaving] = useState(false);
+
+//   const handleSave = () => {
+//     setSaving(true);
+//     setTimeout(() => {
+//       setSaving(false);
+//       toast.success('Історію збережено!');
+//     }, 1000);
+//   };
+
+  const formattedDate = new Date(story.date).toLocaleDateString('uk-UA', {
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+  });
+
+  return (
+    <section className={css.page}>
+      <div className={css.info}>
+        <div className={css.infoDetails}>
+          <div className={css.item}>
+            <p className={css.value}>
+              <strong className={css.label}>Автор статті</strong>{' '}
+              {story.ownerId.name}
+            </p>
+          </div>
+          <div className={css.item}>
+            <p className={css.value}>
+              <strong className={css.label}>Опубліковано</strong>{' '}
+              {formattedDate}
+            </p>
+          </div>
+        </div>
+        <div className={css.infoCategory}>
+          <p className={css.category}>{story.category.name}</p>
+        </div>
+      </div>
+      <Image
+        className={css.image}
+        src={story.img || '/placeholder-image.png'}
+        alt={story.title}
+        width={1312}
+        height={874}
+      />
+      <div className={css.content}>
+        <p className={css.article}>{story.article}</p>
+        <div className={css.saveSection}>
+          <h3 className={css.saveTitle}>Збережіть собі історію</h3>
+          <p className={css.saveText}>
+            {'Вона буде доступна у вашому профілі у розділі збережене.'}
+          </p>
+          <button
+            onClick={handleSave}
+            className={css.saveButton}
+            disabled={saving}
+          >
+            {saving ? 'Збереження...' : 'Зберегти'}
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default StoryDetails;
