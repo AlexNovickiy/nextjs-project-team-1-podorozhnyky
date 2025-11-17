bcffd2006a6d56592de03fcc7a4737e4d9ccc6e9
AlexNovickiy <novickiisasha78@gmail.com>
2025-11-18 01:35:43 +0200

    fix: correct APP_URL constant to use NEXT_PUBLIC_API_URL and fix import statement syntax

--- app/(main-layout)/(public routes)/travellers/[travellerId]/page.tsx
+++ app/(main-layout)/(public routes)/travellers/[travellerId]/page.tsx
@@ -7,7 +7,7 @@ import {
 } from '@tanstack/react-query';
 
 import mainCss from '@/app/Home.module.css';
-import css from '@/app/(main-layout)/(public routes)/travellers/[travellerId]/TravellerId.module.css'
+import css from '@/app/(main-layout)/(public routes)/travellers/[travellerId]/TravellerId.module.css';
 
 import TravellerInfo from '@/components/TravellerInfo/TravellerInfo';
 import MessageNoStories from '@/components/MessageNoStories/MessageNoStories';
@@ -18,7 +18,7 @@ import type { PaginatedStoriesResponse } from '@/types/story';
 
 type PageProps = { params: Promise<{ travellerId: string }> };
 
-const APP_URL = process.env.NEXT_PUBLIC_APP_URL ?? 'http://localhost:3000';
+const APP_URL = process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:3000';
 
 async function fetchTravellerFirstPage(
   travellerId: string
