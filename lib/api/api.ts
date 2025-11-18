f9ac698f1efbf29ac012e14873c30f4c0693974e
ArturMykhalyk <artuhan2015@gmail.com>
2025-11-18 15:33:09 +0200

    add interseptor

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -4,3 +4,54 @@ export const nextServer = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL + '/api',
   withCredentials: true,
 });
+
+let isRefreshing = false;
+let refreshPromise: Promise<boolean> | null = null;
+
+nextServer.interceptors.response.use(
+  response => response,
+
+  async error => {
+    const originalRequest = error.config;
+    if (error.response?.status !== 401) {
+      return Promise.reject(error);
+    }
+    if (originalRequest._retry) {
+      return Promise.reject(error);
+    }
+    originalRequest._retry = true;
+
+    if (isRefreshing) {
+      await refreshPromise;
+      return nextServer(originalRequest);
+    }
+
+    isRefreshing = true;
+
+    refreshPromise = (async () => {
+      try {
+        const res = await fetch('/api/auth/session', {
+          method: 'POST',
+          credentials: 'include',
+        });
+
+        const data = await res.json();
+        return data.success === true;
+      } catch {
+        return false;
+      } finally {
+        isRefreshing = false;
+        refreshPromise = null;
+      }
+    })();
+
+    const ok = await refreshPromise;
+
+    if (ok) {
+      return nextServer(originalRequest);
+    }
+
+    window.location.href = '/auth/login';
+    return Promise.reject(error);
+  }
+);
